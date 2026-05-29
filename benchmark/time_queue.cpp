c8105e68a69fc01aa9e516cd7a45df4e9c7300cb
trigaux <trigaux@cisco.com>
2026-05-29 14:46:28 -0400

    Improve benchmarks.

--- benchmark/time_queue.cpp
+++ benchmark/time_queue.cpp
@@ -127,14 +127,13 @@ BM_TimeQueue_PushAndPopLoaded(benchmark::State& state)
         ++items_count;
         tq.push(items_count, 1000);
 
-        // Simulate load by not poping all items
+        // Simulate load by not popping all items
         if (items_count % 100 == 0) {
             auto elem = tq.front();
             tq.pop();
             benchmark::DoNotOptimize(elem);
+            benchmark::ClobberMemory();
         }
-
-        benchmark::DoNotOptimize(items_count);
     }
 
     state.SetItemsProcessed(items_count);
@@ -153,8 +152,8 @@ BM_TimeQueue_PushAndPop_Interval_1ms(benchmark::State& state)
         auto elem = tq.front();
         tq.pop();
 
-        benchmark::DoNotOptimize(items_count);
         benchmark::DoNotOptimize(elem);
+        benchmark::ClobberMemory();
     }
 
     state.SetItemsProcessed(items_count);
@@ -173,8 +172,8 @@ BM_TimeQueue_PushAndPop_Interval_125ms(benchmark::State& state)
         auto elem = tq.front();
         tq.pop();
 
-        benchmark::DoNotOptimize(items_count);
         benchmark::DoNotOptimize(elem);
+        benchmark::ClobberMemory();
     }
 
     state.SetItemsProcessed(items_count);
@@ -185,8 +184,8 @@ BENCHMARK(BM_TimeQueue_Push)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
 BENCHMARK(BM_TimeQueue_Pop)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
 BENCHMARK(BM_TimeQueue_Front)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
 BENCHMARK(BM_TimeQueue_PopFront)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_Size);
-BENCHMARK(BM_TimeQueue_Empty);
+BENCHMARK(BM_TimeQueue_Size)->Iterations(kIterations);
+BENCHMARK(BM_TimeQueue_Empty)->Iterations(kIterations);
 BENCHMARK(BM_TimeQueue_PushAndPopLoaded)->Arg(5000)->Arg(1'000'000);
 BENCHMARK(BM_TimeQueue_PushAndPop_Interval_1ms)->Arg(5000)->Arg(1'000'000);
 BENCHMARK(BM_TimeQueue_PushAndPop_Interval_125ms)->Arg(5000)->Arg(1'000'000);
