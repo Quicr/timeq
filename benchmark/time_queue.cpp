97dbcaa5521aa882736eb4d15683ab408defbf0e
Tomas Rigaux <trigaux@cisco.com>
2026-06-25 12:35:10 -0400

    Fast Time Queue (#9)
    * MAke another version of the timequeue that doesn't use buckets.
    
    * Change to fast_time_queue.
    
    * Improve benchmarks.
    
    * Remove queue clear duration division by 5.

--- benchmark/time_queue.cpp
+++ benchmark/time_queue.cpp
@@ -1,16 +1,21 @@
 #include <benchmark/benchmark.h>
 
+#include <timeq/fast_time_queue.h>
 #include <timeq/time_queue.h>
+
+#include <type_traits>
 
 static auto service = std::make_shared<timeq::threaded_tick_service>();
 
 constexpr size_t kIterations = 100'000'000;
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_Construct(benchmark::State& state)
+Construct(benchmark::State& state)
 {
+    const std::size_t duration = state.range(0);
     for (auto _ : state) {
-        auto tq = timeq::time_queue<int>(300, 1, service, kIterations);
+        auto tq = TimeQueue(duration, 1, service);
 
         std::size_t size = tq.size();
         benchmark::DoNotOptimize(size);
@@ -18,26 +23,28 @@ BM_TimeQueue_Construct(benchmark::State& state)
     }
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_Push(benchmark::State& state)
+Push(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 1, service, kIterations);
+    TimeQueue tq(state.range(0), 1, service, kIterations);
     int64_t items_count = 0;
 
     for (auto _ : state) {
         ++items_count;
-        tq.push(items_count, 20);
+        tq.push({}, 20);
     }
 
     state.SetItemsProcessed(items_count);
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_Pop(benchmark::State& state)
+Pop(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 1, service, kIterations);
+    TimeQueue tq(state.range(0), 1, service, kIterations);
     for (size_t i = 0; i < kIterations; ++i) {
-        tq.push(i, 10);
+        tq.push({}, 10);
     }
 
     int64_t items_count = 0;
@@ -49,12 +56,13 @@ BM_TimeQueue_Pop(benchmark::State& state)
     state.SetItemsProcessed(items_count);
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_Front(benchmark::State& state)
+Front(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 1, service, kIterations);
+    TimeQueue tq(state.range(0), 1, service, kIterations);
     for (size_t i = 0; i < kIterations; ++i) {
-        tq.push(i, 15);
+        tq.push({}, 15);
     }
 
     int64_t items_count = 0;
@@ -68,12 +76,13 @@ BM_TimeQueue_Front(benchmark::State& state)
     state.SetItemsProcessed(items_count);
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_PopFront(benchmark::State& state)
+PopFront(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 1, service, kIterations);
+    TimeQueue tq(state.range(0), 1, service, kIterations);
     for (size_t i = 0; i < kIterations; ++i) {
-        tq.push(i, 15);
+        tq.push({}, 15);
     }
 
     int64_t items_count = 0;
@@ -87,12 +96,13 @@ BM_TimeQueue_PopFront(benchmark::State& state)
     state.SetItemsProcessed(items_count);
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_Size(benchmark::State& state)
+Size(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(300, 1, service, kIterations);
+    TimeQueue tq(300, 1, service, kIterations);
     for (size_t i = 0; i < kIterations; ++i) {
-        tq.push(i, 10);
+        tq.push({}, 10);
     }
 
     for (auto _ : state) {
@@ -102,12 +112,13 @@ BM_TimeQueue_Size(benchmark::State& state)
     }
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_Empty(benchmark::State& state)
+Empty(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(300, 1, service, kIterations);
+    TimeQueue tq(300, 1, service, kIterations);
     for (size_t i = 0; i < kIterations; ++i) {
-        tq.push(i, 10);
+        tq.push({}, 10);
     }
 
     for (auto _ : state) {
@@ -117,15 +128,16 @@ BM_TimeQueue_Empty(benchmark::State& state)
     }
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_PushAndPopLoaded(benchmark::State& state)
+PushAndPopLoaded(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 1, service, kIterations);
+    TimeQueue tq(state.range(0), 1, service, kIterations);
     int64_t items_count = 0;
 
     for (auto _ : state) {
         ++items_count;
-        tq.push(items_count, 1000);
+        tq.push({}, 1000);
 
         // Simulate load by not popping all items
         if (items_count % 100 == 0) {
@@ -139,15 +151,16 @@ BM_TimeQueue_PushAndPopLoaded(benchmark::State& state)
     state.SetItemsProcessed(items_count);
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_PushAndPop_Interval_1ms(benchmark::State& state)
+PushAndPop_Interval_1ms(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 1, service, kIterations);
+    TimeQueue tq(state.range(0), 1, service, kIterations);
     int64_t items_count = 0;
 
     for (auto _ : state) {
         ++items_count;
-        tq.push(items_count, 1000);
+        tq.push({}, 1000);
 
         auto elem = tq.front();
         tq.pop();
@@ -159,15 +172,16 @@ BM_TimeQueue_PushAndPop_Interval_1ms(benchmark::State& state)
     state.SetItemsProcessed(items_count);
 }
 
+template<class TimeQueue>
 static void
-BM_TimeQueue_PushAndPop_Interval_125ms(benchmark::State& state)
+PushAndPop_Interval_125ms(benchmark::State& state)
 {
-    timeq::time_queue<int> tq(state.range(0), 125, service, kIterations);
+    TimeQueue tq(state.range(0), 125, service, kIterations);
     int64_t items_count = 0;
 
     for (auto _ : state) {
         ++items_count;
-        tq.push(items_count, 1000);
+        tq.push({}, 1000);
 
         auto elem = tq.front();
         tq.pop();
@@ -179,13 +193,67 @@ BM_TimeQueue_PushAndPop_Interval_125ms(benchmark::State& state)
     state.SetItemsProcessed(items_count);
 }
 
-BENCHMARK(BM_TimeQueue_Construct)->Arg(300);
-BENCHMARK(BM_TimeQueue_Push)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_Pop)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_Front)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_PopFront)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_Size)->Iterations(kIterations);
-BENCHMARK(BM_TimeQueue_Empty)->Iterations(kIterations);
-BENCHMARK(BM_TimeQueue_PushAndPopLoaded)->Arg(5000)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_PushAndPop_Interval_1ms)->Arg(5000)->Arg(1'000'000);
-BENCHMARK(BM_TimeQueue_PushAndPop_Interval_125ms)->Arg(5000)->Arg(1'000'000);
+struct TrivialType
+{};
+
+static_assert(std::is_trivially_copyable_v<TrivialType>);
+
+struct NonTrivialType
+{
+    NonTrivialType() {}
+    ~NonTrivialType() {}
+};
+
+static_assert(!std::is_trivially_copyable_v<NonTrivialType>);
+
+using namespace timeq;
+
+BENCHMARK(Construct<time_queue<TrivialType>>)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Construct<time_queue<NonTrivialType>>)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Construct<fast_time_queue<TrivialType>>)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Construct<fast_time_queue<NonTrivialType>>)->Arg(300)->Arg(1'000'000);
+
+BENCHMARK(Push<time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Push<time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Push<fast_time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Push<fast_time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+
+BENCHMARK(Pop<time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Pop<time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Pop<fast_time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Pop<fast_time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+
+BENCHMARK(Front<time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Front<time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Front<fast_time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(Front<fast_time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+
+BENCHMARK(PopFront<time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(PopFront<time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(PopFront<fast_time_queue<TrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+BENCHMARK(PopFront<fast_time_queue<NonTrivialType>>)->Iterations(kIterations)->Arg(300)->Arg(1'000'000);
+
+BENCHMARK(Size<time_queue<TrivialType>>)->Iterations(kIterations);
+BENCHMARK(Size<time_queue<NonTrivialType>>)->Iterations(kIterations);
+BENCHMARK(Size<fast_time_queue<TrivialType>>)->Iterations(kIterations);
+BENCHMARK(Size<fast_time_queue<NonTrivialType>>)->Iterations(kIterations);
+
+BENCHMARK(Empty<time_queue<TrivialType>>)->Iterations(kIterations);
+BENCHMARK(Empty<time_queue<NonTrivialType>>)->Iterations(kIterations);
+BENCHMARK(Empty<fast_time_queue<TrivialType>>)->Iterations(kIterations);
+BENCHMARK(Empty<fast_time_queue<NonTrivialType>>)->Iterations(kIterations);
+
+BENCHMARK(PushAndPopLoaded<time_queue<TrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPopLoaded<time_queue<NonTrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPopLoaded<fast_time_queue<TrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPopLoaded<fast_time_queue<NonTrivialType>>)->Arg(5000)->Arg(1'000'000);
+
+BENCHMARK(PushAndPop_Interval_1ms<time_queue<TrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPop_Interval_1ms<time_queue<NonTrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPop_Interval_1ms<fast_time_queue<TrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPop_Interval_1ms<fast_time_queue<NonTrivialType>>)->Arg(5000)->Arg(1'000'000);
+
+BENCHMARK(PushAndPop_Interval_125ms<time_queue<TrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPop_Interval_125ms<time_queue<NonTrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPop_Interval_125ms<fast_time_queue<TrivialType>>)->Arg(5000)->Arg(1'000'000);
+BENCHMARK(PushAndPop_Interval_125ms<fast_time_queue<NonTrivialType>>)->Arg(5000)->Arg(1'000'000);
