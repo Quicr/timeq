e72e2a4aeb4adc4bc3842b60e231615976d81647
trigaux <trigaux@cisco.com>
2026-08-19 12:31:35 -0400

    Improve compact compile check.

--- include/timeq/time_queue.h
+++ include/timeq/time_queue.h
@@ -392,7 +392,7 @@ namespace timeq {
 
             const std::size_t logical_size = _queue.size() - _queue_index;
 
-            if constexpr (std::is_trivially_copyable_v<T>) {
+            if constexpr (std::is_trivially_copyable_v<queue_value_type>) {
                 std::memmove(_queue.data(), _queue.data() + _queue_index, logical_size * sizeof(queue_value_type));
             } else {
                 std::move(_queue.begin() + static_cast<std::ptrdiff_t>(_queue_index), _queue.end(), _queue.begin());
